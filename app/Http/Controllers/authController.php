313c2718a66bc04aafbc50b560c82989be65ef95
andikapp98 <andikapp1998@gmail.com>
2023-01-06 16:45:57 +0700

    crud

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Auth;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Laravel\Socialite\Facades\Socialite;
 
 class authController extends Controller
@@ -27,14 +28,21 @@ class authController extends Controller
         $id = $user->id;
         $email = $user->email;
         $name = $user->name;
+        $avatar = $user->avatar;
+
+
         // cek email google 
         $cek = User::where('email', $email)->count();
         if($cek > 0){
+            $avatar_file = $id . ".jpg";
+            $fileContent = file_get_contents($avatar);
+            File::put(public_path("admin/images/faces/$avatar_file"), $fileContent);
             $user = User::updateOrCreate(
                 ['email' =>$email],
                 [
                     'name' =>$name,
-                    'google_id' =>$id
+                    'google_id' =>$id,
+                    'avatar' => $avatar_file
                 ]
             );
             Auth::login($user);
